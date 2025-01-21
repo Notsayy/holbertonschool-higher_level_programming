478b3caa1938895ea03d201eaf72ef123ce316b6
Notsayy <jtertre25@gmail.com>
2025-01-21 17:53:32 +0100

    Fix an issue.

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copy = my_list[:]
     if idx < 0:
         return copy
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return copy
     copy[idx] = element
     return copy
