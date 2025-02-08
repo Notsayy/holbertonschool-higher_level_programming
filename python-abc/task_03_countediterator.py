f4bbf605bf0d6a2b4e8611abd170f3891b3f2428
Notsayy <jtertre25@gmail.com>
2025-02-08 14:20:03 +0100

    Add the task 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -4,12 +4,10 @@ class CountedIterator:
         self.iterator = iter(iterable)
         self.count = 0
 
-    def __iter__(self):
-        return self
-
     def __next__(self):
+        item = next(self.iterator)
         self.count += 1
-        return next(self.iterator)
+        return item
 
     def get_count(self):
         return self.count
