417d0a7c7c830897740fd7f19fe38df4f0333339
Notsayy <jtertre25@gmail.com>
2025-03-07 12:36:27 +0100

    Add the checking requirements

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,11 @@ import sys
 
 # Ensures the script runs only if executed directly
 if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
 
     # Retrieve command-line arguments.
     username = sys.argv[1]
