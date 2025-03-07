3b221b01b8b3d90d55a11aaa749d68c96955896b
Notsayy <jtertre25@gmail.com>
2025-03-07 12:15:49 +0100

    Forget to change comments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
 This script connects to a MySQL database and retrieves all states
-from the `states` table whose name starts with 'N',
+from the `states` table where the name matches the given argument,
 ordered by `id` in ascending order.
 
 Usage:
-    ./script_name.py <mysql_username> <mysql_password> <database_name>
+    ./script_name.py <mysql_username> <mysql_password> <database_name> <state_name>
 """
 
 import MySQLdb
@@ -14,16 +14,17 @@ import sys
 # Ensures the script runs only if executed directly
 if __name__ == "__main__":
     # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>"
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql username> <mysql password> <database name> <state name>"
               .format(sys.argv[0]))
         sys.exit(1)
 
-    # Retrieve command-line arguments (MySQL username, password, database name)
+    # Retrieve command-line arguments (MySQL username, password, database name, state name)
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
+    state_name = sys.argv[4]  # The state name to search for
+
     # Establish a connection to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
@@ -36,10 +37,10 @@ if __name__ == "__main__":
     # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch states whose name starts with 'N'
+    # Execute the SQL query to fetch states where the name matches the given state_name
     cursor.execute(
         "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,)
-        )
+    )
 
     # Fetch all results from the executed query
     results = cursor.fetchall()
